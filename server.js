698d3be9ec7c93d06bdfbad419ac70b7e0aa50ef
Michael Holod <git_ara@ukr.net>
2018-03-25 19:09:51 +0300

    server.js structured and Routers have transferred to separate route file

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-const db = require("./config/db");
+const baseRouts = require("./routes/base");
 
 //Set template engine
 app.set('view engine','pug');
 //Set folder storage of Views
 app.set('views',__dirname+'/views');
 //Static files storage: 1.URL segment, 2.Folder name.
-app.use('/public',express.static('public'));
+app.use('/public',express.static(path.join(__dirname,'public')));
 
-app.get("/",(request,response)=>{
-	//response.send("<h1>Hello</h1>");
-	var d1 = db();
-	global.d1 = d1[0];
-	response.render("index",{title:"My Index",result:d1[1]});
-});
-app.get("/account",(request,response)=>{
-	var d2 = db("second","info");
-	d2[0].insert({name : "Boris the Blade", year: 1946});
-	response.render("account",{title:"My account",result:d2[1]+" "+(global.d1===d2[0])});
-});
+//Middleware for Base routing registration
+app.use("/",baseRouts);
+
 //const port = Number(process.env.PORT || 3000);
 app.listen(3000,()=>{
 	console.log("Listening on port 3000.");
