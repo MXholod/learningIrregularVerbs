0c5d7fc2a24a7dd23fca7f14101eccc3de6b2a05
Michael Holod <git_ara@ukr.net>
2018-04-08 00:44:07 +0300

    Authorizing panel has sliding gradient

--- routes/base.js
+++ routes/base.js
@@ -4,14 +4,14 @@ let routes = express.Router();
 
 routes.get("/",(request,response)=>{
 	//response.send("<h1>Hello</h1>");
-	var d1 = db();
-	global.d1 = d1[0];
-	response.render("index",{title:"My Index",result:d1[1]});
+	//var d1 = db();
+	//global.d1 = d1[0];
+	response.render("index",{title:"Irregular verbs"});//,result:d1[1]
 });
 routes.get("/account",(request,response)=>{
-	var d2 = db("second","info");
-	d2[0].insert({name : "Boris the Blade", year: 1946});
-	response.render("account",{title:"My account",result:d2[1]+" "+(global.d1===d2[0])});
+	//var d2 = db("second","info");
+	//d2[0].insert({name : "Boris the Blade", year: 1946});
+	response.render("account",{title:"My account"});//,result:d2[1]+" "+(global.d1===d2[0])
 });
 
 module.exports = routes; 
