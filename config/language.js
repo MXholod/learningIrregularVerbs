7f034617f0f51bac20d24ac8a731d3e8cbd9516a
Michael Holod <git_ara@ukr.net>
2018-04-10 23:27:20 +0300

    Localization file created for auth and auth styles changed

---
+++ config/language.js
@@ -0,0 +1,37 @@
+module.exports = {
+	"ru":{
+		"mainpage":{
+			"auth":{
+				"formTitle":"Выполните регистрацию",
+				"firstControl":{
+					"labelTitle":"Введите имя",
+					"inputValue":"Ваше имя"
+				},
+				"secondControl":{
+					"labelTitle":"Введите пароль",
+					"placeHolder":"Ваш пароль"
+				},
+				"submit":"Отправить",
+				"forgetPassLabel":"Забыли пароль?"
+			}
+		}
+	},
+	"ua":{
+		"mainpage":{
+			"auth":{
+				"formTitle":"Виконайте реєстрацію",
+				"firstControl":{
+					"labelTitle":"Введіть ім'я",
+					"inputValue":"Ваше ім'я"
+				},
+				"secondControl":{
+					"labelTitle":"Введіть пароль",
+					"placeHolder":"Ваш пароль"
+				},
+				"submit":"Відправити",
+				"forgetPassLabel":"Забули пароль?"
+			}
+		}
+	}
+	
+};
--- routes/base.js
+++ routes/base.js
@@ -1,12 +1,16 @@
 const db = require("../config/db");
+const lang = require("../config/language");
 const express = require("express");
 let routes = express.Router(); 
-
+/*let language;
+if(lang.ru !== "ru"){
+	language = lang.ru;
+}*/
 routes.get("/",(request,response)=>{
 	//response.send("<h1>Hello</h1>");
 	//var d1 = db();
 	//global.d1 = d1[0];
-	response.render("index",{title:"Irregular verbs"});//,result:d1[1]
+	response.render("index",{title:"Irregular verbs",lang:lang.ua});//,result:d1[1]
 });
 routes.get("/account",(request,response)=>{
 	//var d2 = db("second","info");
