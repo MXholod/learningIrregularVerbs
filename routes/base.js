7feda75692c8fd8b3b3c85f4689e21c68c7b65d3
Michael Holod <git_ara@ukr.net>
2018-04-20 18:03:13 +0300

    SVG words on main page completed settings block menu started

--- routes/base.js
+++ routes/base.js
@@ -10,7 +10,7 @@ routes.get("/",(request,response)=>{
 	//response.send("<h1>Hello</h1>");
 	//var d1 = db();
 	//global.d1 = d1[0];
-	response.render("index",{title:"Irregular verbs",lang:lang.ua});//,result:d1[1]
+	response.render("index",{title:"Irregular verbs",lang:lang.ru});//,result:d1[1]
 });
 routes.get("/account",(request,response)=>{
 	//var d2 = db("second","info");
