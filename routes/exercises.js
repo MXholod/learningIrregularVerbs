890a6fe20f04410e99fe1edff37dfe7ccd93cba4
Michael Holod <git_ara@ukr.net>
2019-03-14 20:28:52 +0200

    Rendering correct data when going by pages

--- routes/exercises.js
+++ routes/exercises.js
@@ -18,9 +18,13 @@ routes.get("/method1",(request,response)=>{
 		//Create object with IDs and spoiled IDs: {IDs:[...],spoiledPortionsIDs:[[...],[...],[...],[...],[...]]}
 		//Call this Method only once. 50 and 25. Divide to spoiled and normals IDs for all 5 pages.
 			let objectIDs = method_1.cutForSpoiledIDs(arrUniqueNums);
+
+		let t1 = objectIDs.IDs.length;
 		//The first parameter is an Object with all IDs and the second is GET parameter.
 		//Return Array with [['Normal with spoiled'],[only normal],[only spoiled]]
-			let threeArrays = method_1.getPortionIDs(objectIDs,request.query.currentPageAmount);
+			let threeArrays = method_1.getPortionIDs(objectIDs,request.query.currentPageAmount);	
+		
+		let t2 = objectIDs.IDs.length;
 		//Serialize data, last 40 rows and spoiled
 			let serializedArray = JSON.stringify(objectIDs);
 		//Save to Session 40 rows
@@ -41,9 +45,11 @@ routes.get("/method1",(request,response)=>{
 					userLoginSession : request.session.login,
 					title:"Method 1",
 					randomRows: mixedWords,//
+					currentNum: (Number(request.query.currentPageAmount)+1),//Query string argument for next page
 					test : threeArrays[0].join(",")+" - "+threeArrays[0].length,
 					test2 : threeArrays[1].join(",")+" - "+threeArrays[1].length,
-					test3 : threeArrays[2].join(",")+" - "+threeArrays[2].length
+					test3 : threeArrays[2].join(",")+" - "+threeArrays[2].length,
+					test4 : "Result: t1 "+t1+" t2 "+t2
 				});
 			}else{
 				request.session.numbers = "";
@@ -55,10 +61,14 @@ routes.get("/method1",(request,response)=>{
 		if(request.session.numbers){
 			//Array with [['Normal with spoiled'],[only normal],[only spoiled]] 
 			//Unique numbers from Session 40,30,20,10
-			/*let sessionObjectIDs = JSON.parse(request.session.numbers);
+			let sessionObjectIDs = JSON.parse(request.session.numbers);
+			
+			let t1 = sessionObjectIDs.IDs.length;
 				//The first parameter is an Object with all IDs and the second is GET parameter.
 				//Return Array with [['Normal with spoiled'],[only normal],[only spoiled]]
 				let threeArrays = method_1.getPortionIDs(sessionObjectIDs,request.query.currentPageAmount);
+				
+			let t2 = sessionObjectIDs.IDs.length;	
 				//Serialize data, last 40 rows and spoiled
 				let serializedArray = JSON.stringify(sessionObjectIDs);
 				//Save to Session 30 rows
@@ -66,25 +76,28 @@ routes.get("/method1",(request,response)=>{
 				//Request to DB
 			dbs.databases.verbs.find({_id:{$in:threeArrays[0]}},function(err, docs){
 				if(docs.length > 0){
-				//Create an Array of data objects [{translatedWord:'ru|ua word',engArray:['e1','e2','e3'],id:1},{},..]
+			//Create an Array of data objects [{translatedWord:'ru|ua word',engArray:['e1','e2','e3'],id:1},{},..]
 					let templateData = helpers.exercises.arrayOfTasks(docs,language,true);
 					//Set specific property 'spoiled' for each object.
 					let templateDataSpoiled = method_1.preparedVerbs(templateData,threeArrays);
 					//Elements of Data Array are equal to random numbers Array. Objects in random order. 
 					let mixedWords = helpers.exercises.changePositions(threeArrays[1],templateDataSpoiled);
-					response.render("method-1",{
+					response.render("method-1-walk-through",{
 						userLoginSession : request.session.login,
 						title:"Method 1",
-						randomRows:mixedWords,
-						test : threeArrays[0],
-						test2 : threeArrays[1],
-						test3 : sessionObjectIDs.IDs.length //templateDataSpoiled[0].spoiled
+						randomRows: mixedWords,//
+						//Query string argument for next page
+						currentNum: (Number(request.query.currentPageAmount)+1),
+						test : threeArrays[0].join(",")+" - "+threeArrays[0].length,
+						test2 : threeArrays[1].join(",")+" - "+threeArrays[1].length,
+						test3 : threeArrays[2].join(",")+" - "+threeArrays[2].length,
+						test4 : "Result: t1 "+t1+" t2 "+t2
 					});
 				}else{
 					request.session.numbers = "";
 					redirect(301,'/exercises');
 				}
-			}	*/
+			});	
 		}
 	}
 });
