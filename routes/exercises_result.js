49a23667eb22c458eaca882b1d69cdb4d1c749eb
Michael Holod <git_ara@ukr.net>
2019-06-13 20:46:02 +0300

    Methods Result Template, Router, Logic files created

---
+++ routes/exercises_result.js
@@ -0,0 +1,12 @@
+const express = require("express");
+let routes = express.Router();
+
+routes.get("/show-result",(request,response)=>{
+	response.render("result-show",{
+		userLoginSession : request.session.login,
+		title:"Show results",
+		result: "Result is ..."
+	});
+});
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const baseRouts = require("./routes/base");
 const profileRouts = require("./routes/profile");
 const exercises = require("./routes/exercises");
+const results = require("./routes/exercises_result");
 const server = require("./libs/io")(app);
 
 //Set template engine
@@ -19,7 +20,8 @@ app.set('views',[
 	__dirname+'/views/main-page',
 	__dirname+'/views/profile-page',
 	__dirname+'/views/listVerbs-page',
-	__dirname+'/views/exercises-page'
+	__dirname+'/views/exercises-page',
+	__dirname+'/views/exercises-page-results'
 ]);
 //Static files storage: 1.URL segment, 2.Folder name.
 app.use('/public',express.static(path.join(__dirname,'public')));
@@ -40,6 +42,8 @@ app.use("/",baseRouts);
 app.use("/",profileRouts);
 //Middleware for Exercises routing registration
 app.use("/",exercises);
+//Middleware for Results routing registration
+app.use("/",results);
 
 //const port = Number(process.env.PORT || 3000);
 server.listen(3000,()=>{//"127.0.0.1",
